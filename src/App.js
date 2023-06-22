f59b8bb67504765baedb0cefb70c47cc63bcc4db
reposektora <122121001+reposektora@users.noreply.github.com>
2023-06-22 12:03:42 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         <NavScrollTop>
           <Suspense fallback={<div />}>
                 <Routes>
-                  <Route path={`${process.env.PUBLIC_URL + "/"}`} element={<HomeOne/>}/>
+                  <Route path={`${process.env.PUBLIC_URL + "/home-three"}`} element={<HomeOne/>}/>
                   {/* <Route path={`${process.env.PUBLIC_URL + "/home-one"}`} element={<HomeOne/>}/> */}
                   {/* <Route path={`${process.env.PUBLIC_URL + "/home-two"}`} element={<HomeTwo/>}/> */}
                   /*<Route path={`${process.env.PUBLIC_URL + "/home-three"}`} element={<HomeThree/>}/>*/
